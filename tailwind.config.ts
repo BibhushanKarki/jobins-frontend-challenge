a54dbcbf9c7b269cf613cd9a1c41a2bf2ea622b0
Bibhushan Karki <bibhushan.karki11@gmail.com>
2024-09-26 11:47:45 +0545

    fixed tailwind errors and added fluid-tailwind package

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,11 @@
 import { Config } from 'tailwindcss';
+import fluid, { extract } from 'fluid-tailwind';
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  extract,
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [fluid],
 } satisfies Config;
